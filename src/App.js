ea0e089269a5c59f1ccef540483dea4e5d9443cc
LABDesenvolver <gui_wb@hotmail.com>
2024-06-07 11:38:56 -0300

    add deploy vercel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
